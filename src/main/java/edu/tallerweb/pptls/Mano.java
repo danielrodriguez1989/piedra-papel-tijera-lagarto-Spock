9a98072395ad444fd86cae27e6f49ddc5aff154f
unknown <daniel.r1989@hotmail.com>
2013-10-02 21:18:41 -0300

    sad

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -8,19 +8,23 @@ import java.util.ArrayList;
 public class Mano {
 	private Forma valorForma = null;
 	private String nombreJugador = null;
+
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
+	 * Toda Mano debe crearse con una forma dada, que será la que determine su
+	 * condición en el juego.
+	 * 
+	 * @param forma
+	 *            , la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
 		this.valorForma = forma;
 	}
 
 	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
+	 * Evaluará el resultado de la partida según las reglas del juego.
+	 * 
+	 * @param otra
+	 *            , la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
@@ -61,12 +65,15 @@ public class Mano {
 			}
 		}
 	}
+
 	public String getNombre() {
 		return this.nombreJugador;
 	}
+
 	public void setNombre(String nom) {
 		this.nombreJugador = nom;
 	}
+
 	public Forma getForma() {
 		return this.valorForma;
 	}
